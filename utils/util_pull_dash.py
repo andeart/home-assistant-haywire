d88b073282fd20d730817bc84da725651fd7b711
Anurag Devanapally <anurag.devanapally@gmail.com>
2026-03-04 23:36:17 -0800

    Fix naming in util_pull_dash.py.

--- utils/util_pull_dash.py
+++ utils/util_pull_dash.py
@@ -5,7 +5,7 @@ import sys
 
 import websockets
 
-async def get_config(url_path):
+async def pull_config(url_path):
     async with websockets.connect("ws://homeassistant.local:8123/api/websocket") as ws:
         await ws.recv()  # auth_required
         await ws.send(json.dumps({"type": "auth", "access_token": os.environ["HA_UI_EDIT_TOKEN"]}))
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"Usage: {sys.argv[0]} <url_path>", file=sys.stderr)
         sys.exit(1)
-    asyncio.run(get_config(sys.argv[1]))
+    asyncio.run(pull_config(sys.argv[1]))
